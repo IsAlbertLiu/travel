25ac05794b0e541920b0d0219046be87a2174184
albertliu <albertliu310@gmail.com>
2020-08-07 14:47:44 +0800

    index header and swiper

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'swiper/dist/css/swiper.css'
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 引入css文件
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import 'styles/reset.css'
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        This is Header
-    </div>
+<div class="wrapper">
+ <div class="back-icon">
+   <span class="iconfont">&#xe624;</span>
+</div>
+<div class="search">
+   输入城市/景点/游玩主题
+   <span class="iconfont search-icon">&#xe632;</span>
+</div>
+<div class="city">
+   黄冈
+   <span class="iconfont">&#xe64a;</span>
+</div>
+</div>
 </template>
 
 <script>
@@ -9,3 +19,33 @@ export default {
   name: 'HomeHeader'
 }
 </script>
+
+<style lang="stylus" scoped>
+.wrapper
+     height .64rem
+     background #00bcd4
+     color white
+     line-height .64rem
+ .back-icon
+     float left
+     width .64rem
+     text-align center
+ .search
+     float left
+     height  .44rem
+     background #fff
+     color gray
+     line-height .44rem
+     border-radius .1rem
+     margin  .1rem .1rem
+     font-size .2rem
+     padding  0 2rem
+     position relative
+     .search-icon
+         position absolute
+         left .1rem
+         top 0rem
+ .city
+     width 1rem
+     float right
+</style>
